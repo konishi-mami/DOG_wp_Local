f6203e1de64979c0ac397620add4dde062a4e572
konishi-mami <hamikoma0528@gmail.com>
2025-08-04 11:31:42 +0900

    8.4_start

---
+++ index.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>DOG_wp</title>
+  <meta name="description" content="Dog">
+  <link rel="stylesheet" href="pc.css">
+  <link rel="stylesheet" href="sp.css">
+  <link rel="stylesheet" href="reset.css">
+</head>
+
+<body>
+  <header class="header">
+    <h1 class="header__logo"><img src="./img/header__logo.png" alt="ロゴロゴ"></h1>
+      <nav class="gnav">
+        <ul class="gnav__list">
+          <li class="gnav__item"><a href="#">Dog1</a></li>
+          <li class="gnav__item"><a href="#">Dog2</a></li>
+          <li class="gnav__item"><a href="#">Dog3</a></li>
+        </ul>
+      </nav>
+  </header>
+
+  <main>
+    <div class="mv">
+    </div>
+  <div class="container">
+    <div class="container-A">
+      <h2 class="container-A__title">犬は三日飼うと三年恩を忘れない。
+      </h2>
+      <p class="text-area__DogA">その頃、本所から四谷箪笥町たんすまち、<br>
+        芝片門前、三田の赤羽橋辺まで<br>
+        襖を積んだ車を引いて使いにやらされる</p>
+      <p class="text-area__DogA">行きにその犬を車の梶棒へ綱でつなぐとグングン<br>
+        車を引いてくれる。先方へ行って使い賃をもらうと、<br>
+        パンを買って半分は犬に食べさせて、</p>
+      <p class="text-area__DogA">空になった車へ犬を載せて引いてくる。<br>
+        今でもバタヤさんが</p>
+    </div>
+  </div>
+  <section class="Dog1">
+  <div class="container">
+        <h3 class="Dog1__title">DOG1</h3>
+          <ul class="Dog1List">
+            <li class="Dog1List__item">
+              <div class="Dog1List__thumb"><img src="./img/Dog_img_01.png" alt="犬001"></div>
+              <div class="Dog1List__text">
+                <dl class="Dog1__subtitle">犬は三日飼うと三年恩を忘れない。</dl>
+                <dd class="Dog1__text-area">ぼくがあまり犬を可愛がりすぎるので、伯父がぼくに内緒でどこかへ捨ててきたらしい。二、三日の間は仕事も手につかず、食べる物もうまくない。それこそ血眼になって探したがわからない。</dd>
+              </div>
+            </li>
+            <li class="Dog1List__item">
+              <div class="Dog1List__thumb"><img src="./img/Dog_img_02.png" alt="犬002"></div>
+              <div class="Dog1List__text">
+                <dl class="Dog1__subtitle">犬は三日飼うと三年恩を忘れない。</dl>
+                <dd class="Dog1__text-area">去る者は日々に疎し、というか、二月、三月とすぎ、半 年もすぎるとまるで思いださないというより、どんな犬 であったか思いだせないくらいになっていた。</dd>
+              </div>
+            </li>
+            <li class="Dog1List__item">
+              <div class="Dog1List__thumb"><img src="./img/Dog_img_03.png" alt="犬003"></div>
+              <div class="Dog1List__text">
+                <dl class="Dog1__subtitle">犬は三日飼うと三年恩を忘れない。</dl>
+                <dd class="Dog1__text-area">すると或る日、京橋八丁堀まで例によって車を引いてゆ き、帰りに、その頃「中外商業新聞」というのがあっ て、その前の坂本公園という、いやに淋しい公園の前ま でくると、だし抜けにぼくに飛びついた犬がある。</dd>
+              </div>
+            </li>
+          </ul>
+  </div>
+  </section>
+  <section class="Dog2">
+  <div class="container">
+      <h3 class="Dog2__title">DOG2</h3>
+        <ul class="Dog2List">
+          <li class="Dog2List__item">
+          <div class="Dog2List__thumb"><img src="./img/Dog_img_04.png" alt="犬004"></div>
+              <p class="text-area__Dog2">びっくりして見ると、半年ほど前にわが 子のように可愛がったその犬である。</p>
+            </li>
+            <li class="Dog2List__item">
+            <div class="Dog2List__thumb"><img src="./img/Dog_img_05.png" alt="犬005"></div>
+              <p class="text-area__Dog2">犬も夢中になって飛びつく、ぼくも暫時 われを忘れて抱きかかえて、</p>
+            </li>
+            <li class="Dog2List__item">
+            <div class="Dog2List__thumb"><img src="./img/Dog_img_06.png" alt="犬006"></div>
+              <p class="text-area__Dog2">また改めて伯父に頼んで飼ってもらった が、その犬の顔は今でも眼をつぶると瞼のうちに見えるようだ。</p>
+            </li>
+        </ul>
+  </div>
+  </section>
+  </div>
+  <div class="container-cover">
+    <div class="overray__text">その頃、本所から四谷箪笥町たんすまち、<br>
+          芝片門前、三田の赤羽橋辺まで<br>
+          襖を積んだ車を引いて使いにやらされる<br><br><br>行きにその犬を車の梶棒へ綱でつなぐとグングン<br>
+          車を引いてくれる。先方へ行って使い賃をもらうと、<br>
+          パンを買って半分は犬に食べさせて、
+    </div>
+  </div>
+  <section class="Dog3">
+  <div class="container">
+      <h3 class="Dog3__title">DOG3</h3>
+        <ul class="Dog3List">
+          <li class="Dog3List__item">
+            <div class="info">
+              <span class="Dog3__date">2018.02.09</span>
+              <span class="Dog3__New">新着</span>
+            </div>
+            <div class="Dog3__text">とんだの犬の話になったが、いろいろの種類の犬も飼ってみたが</div>
+          </li>
+        <ul class="Dog3List">
+          <li class="Dog3List__item">
+            <div class="info">
+              <span class="Dog3__date">2019.03.05</span>
+              <span class="Dog3__New">新着</span>
+            </div>
+            <div class="Dog3__text">何の何種という系統正しい犬でも、名もない雑種の野良犬でも、飼えば同じように可愛いもの</div>
+          </li>
+        <ul class="Dog3__List">
+          <li class="Dog3List__item">
+            <div class="info">
+              <span class="Dog3__date">2021.01.05</span>
+              <span class="Dog3__New">新着</span>
+            </div>
+            <div class="Dog3__text">若い時分、駄犬のことで犬捕りの人夫と殴りあいの喧嘩をしたことがある</div>
+          </li>
+        <ul class="Dog3__List">
+          <li class="Dog3List__item">
+            <div class="info">
+              <span class="Dog3__date">2022.06.10</span>
+              <span class="Dog3__New">新着</span>
+            </div>
+            <div class="Dog3__text">本所太平町に住んでいる時分、下手な鉄砲をやっていたので</div>
+          </li>
+        <ul class="Dog3__List">
+          <li class="Dog3List__item">
+            <div class="info">
+              <span class="Dog3__date">2023.10.10</span>
+              <span class="Dog3__New">新着</span>
+            </div>
+            <div class="Dog3__text">ポインターの猟犬を飼っていたが、これも子犬からもらってきて</div>
+          </li>
+        </ul>
+        <button class="button">
+          <a href="#" class="cta-btn">MORE</a>
+        </button>
+    </div>
+    </section>
+
+  <footer class="footer">
+    <div class="footer__content">
+    <div class="footerListBlock">
+      <p class="footer__title">犬は利口なもので、</p>
+        <ul class="footerList__group">
+          <li class="footerList__item"> 昼席、夜席へ行くときの服装 </li>
+          <li class="footerList__item"> 魚釣りの服装ででかけるとき </li>
+          <li class="footerList__item"> また、鉄砲を持って犬を連れてゆ </li>
+          <li class="footerList__item"> 獲物がいると犬はすぐにポイントをする </li>
+          <li class="footerList__item"> 銃の仕度をして </li>
+        </ul>
+    </div>
+    <div class="footerListBlock">
+      <p class="footer__title">犬は利口なもので、</p>
+        <ul class="footerList__group">
+          <li class="footerList__item"> 昼席、夜席へ行くときの服装 </li>
+          <li class="footerList__item"> 魚釣りの服装ででかけるとき </li>
+          <li class="footerList__item"> また、鉄砲を持って犬を連れてゆ </li>
+          <li class="footerList__item"> 獲物がいると犬はすぐにポイントをする </li>
+          <li class="footerList__item"> 銃の仕度をして </li>
+        </ul>
+    </div>
+    <div class="footerListBlock">
+      <p class="footer__title">犬は利口なもので、</p>
+        <ul class="footerList__group">
+          <li class="footerList__item"> 昼席、夜席へ行くときの服装 </li>
+          <li class="footerList__item"> 魚釣りの服装ででかけるとき </li>
+          <li class="footerList__item"> また、鉄砲を持って犬を連れてゆ </li>
+          <li class="footerList__item"> 獲物がいると犬はすぐにポイントをする </li>
+          <li class="footerList__item"> 銃の仕度をして </li>
+        </ul>
+    </div>
+    </div>
+    <p class="copylight">Copyright 2019 saigomadeganbatta.</p>
+  </footer>
+  </main>
+
+</body>
+
+</html>
